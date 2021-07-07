2ee88b9ebbcaa56f3511eba355af47a2fe05b861
hhio618 <hhio618@gmail.com>
2021-07-07 21:43:19 +0000

    update deps

--- src/api/query_runner.js
+++ src/api/query_runner.js
@@ -1,6 +1,6 @@
 import axois from "axios";
 export function runInfluxQuery(query) {
-  const url = "https://onebeaconme.live/api/v1/query";
+  const url = "https://onebeaconme.tech/api/v1/query";
   return new Promise((resolve) => {
     axois
       .post(url, query, {
